4f50526f5285cc585dec1a7a53a9adc9ceb34e33
Haoran Zhang - h0z02yc <haoran.zhang@walmart.com>
2021-02-05 23:03:29 +0000

    Project init

---
+++ server/index.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import exampleData from "./exampleData.json";
+
+const app = express();
+
+const PORT = process.env.PORT || 8000;
+
+app.get("/", (req, res) => res.send("Express + TypeScript Server"));
+
+const simulateDateFetching = async (delay: number) => {
+  await new Promise((r) => setTimeout(r, delay));
+  return exampleData;
+};
+
+app.get("/api/posts", async (req, res) => res.send(await simulateDateFetching(200)));
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
